ba936052db8e458d908619efcf88ea9bfb5113b9
Akif AYDIN <akifaydin52@gmail.com>
2020-08-02 19:00:05 +0300

    updated

---
+++ Chapter_04/exercises/4.27/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int binaryToDecimal(int bin);
+
+int main(int argc, char const *argv[])
+{
+    int binary{0};
+
+    std::cout << "Printing the decimal equivalent of a binary number"
+              << std::endl
+              << std::endl;
+
+    std::cout << "Enter a binary number for decimal conversion: ";
+    std::cin >> binary;
+
+    std::cout << binary
+              << " as decimal = "
+              << binaryToDecimal(binary)
+              << std::endl;
+
+    return 0;
+}
+
+int binaryToDecimal(int bin)
+{
+    int decimal{0}, value{1};
+
+    while (bin != 0)
+    {
+        decimal += (bin % 10) * value;
+        bin /= 10;
+        value += value;
+    }
+    return decimal;
+}
