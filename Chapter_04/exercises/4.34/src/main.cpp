c9e80c6ffc0f647c3d0311f7abac3560b3e3b186
Akif AYDIN <akifaydin52@gmail.com>
2020-08-10 17:34:52 +0300

    Updated

---
+++ Chapter_04/exercises/4.34/src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int getFactorial(int);
+double getEuler(int);
+
+int main(int argc, char const *argv[])
+{
+    int number;
+    std::cout << "A Program to determine the factorial of nonzero integers"
+              << std::endl;
+    std::cout << "Enter a nonzero integer: ";
+    std::cin >> number;
+    std::cout << number
+              << "! = "
+              << getFactorial(number)
+              << std::endl;
+    return 0;
+}
+
+int getFactorial(int n)
+{
+    int factorial{1}, counter{1};
+
+    while (n > 0)
+    {
+        factorial *= n;
+        n--;
+    }
+
+    return factorial;
+}
+
+double getEuler(int a)
+{
+    int accuracy;
+    std::cout << "debenem"
+              << std::endl;
+}
