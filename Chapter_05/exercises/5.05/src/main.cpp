71df5b5d0a69f3269c65e8ea5eefaaaf8e17dba9
Akif AYDIN <akifaydin52@gmail.com>
2020-08-14 17:48:53 +0300

    Updated

--- Chapter_05/exercises/5.05/src/main.cpp
+++ Chapter_05/exercises/5.05/src/main.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-   
+    int num, total{0}, count;
 
+    std::cout << "Enter the number of values to be summed followed by values: ";
+    std::cin >> count;
+
+    for (int i = 1; i <= count; ++i)
+    {
+        std::cin >> num;
+        total += num;
+    }
+
+    std::cout << "Total = " << total << std::endl;
     return 0;
 }
--- Chapter_05/exercises/5.06/src/main.cpp
+++ Chapter_05/exercises/5.06/src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
+int average(int, int);
+
+static const int EXIT = 9999;
 int main()
 {
-   
+    int counter{0};
+    int sum{0};
+
+    std::cout << "Enter a list of space separated integers for average calculation. "
+              << EXIT << " ends input" << std::endl;
+    std::cin >> sum;
+
+    for (int i = 0; i != EXIT; ++counter)
+    {
+        sum += i;
+        std::cin >> i;
+    }
+
+    std::cout << "Average of sum: " << average(sum, counter) << std::endl;
 
     return 0;
+}
+
+int average(int a, int b)
+{
+    return (a / b);
 }
