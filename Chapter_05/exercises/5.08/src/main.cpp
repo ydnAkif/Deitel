bd03f965df413ec9db77efce35c11e2c7af60d24
Akif AYDIN <akifaydin52@gmail.com>
2020-08-14 21:30:33 +0300

    Updated

--- Chapter_05/exercises/5.08/src/main.cpp
+++ Chapter_05/exercises/5.08/src/main.cpp
@@ -2,7 +2,26 @@
 
 int main()
 {
-   
+    int counter{0};
+    int smallest{0};
+    int current{0};
+
+    std::cout << "Enter a list of space separated integers to determine smallest one."
+              << std::endl;
+    std::cout << "First input is number of separate entries..." << std::endl;
+    std::cin >> counter;
+
+    for (size_t i = 0; i < counter; ++i)
+    {
+        std::cin >> current;
+
+        if ((current < smallest) || (i == 0))
+        {
+            smallest = current;
+        }
+    }
+
+    std::cout << "Smallest one is " << smallest << std::endl;
 
     return 0;
 }
--- Chapter_05/exercises/5.09/src/main.cpp
+++ Chapter_05/exercises/5.09/src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+// 1*3*5*7*9*11*13*15
+
 int main()
 {
-   
+    int productOdd = 1;
+
+    for (int i = 1; i <= 15; i += 2)
+    {
+        productOdd *= i;
+    }
+
+    std::cout << "the product of the odd integers from 1 to 15 = " << productOdd << std::endl;
 
     return 0;
 }
