975dd20f04c23acc3a8e335aac3c67982f2b2d4e
Akif AYDIN <akifaydin52@gmail.com>
2020-08-15 15:27:55 +0300

    Updated

--- Chapter_05/exercises/5.13/src/main.cpp
+++ Chapter_05/exercises/5.13/src/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
+void printLine(int);
 int main()
 {
-   
+    int counter{5};
+    int value{0};
+
+    std::cout << "Program to print bar chart of entered values\n"
+              << std::endl;
+    while (counter > 0)
+    {
+        std::cout << "Enter an integer between 1-30 ("
+                  << counter
+                  << " remaining): ";
+
+        std::cin >> value;
+
+        if (value >= 1 && value <= 30)
+        {
+            printLine(value);
+            counter--;
+        }
+        else
+        {
+            std::cout << "Incorrect value entered!"
+                      << std::endl;
+        }
+    }
 
     return 0;
+}
+
+void printLine(int value)
+{
+    for (int i = 0; i < value; ++i)
+    {
+        std::cout << "*";
+    }
+    std::cout << std::endl;
 }
