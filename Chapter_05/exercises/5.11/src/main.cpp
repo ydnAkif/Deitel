6a6e7af380f3ef6edb3f97a87d0a05cd484e806d
Akif AYDIN <akifaydin52@gmail.com>
2020-08-15 15:16:58 +0300

    Updated

--- Chapter_05/exercises/5.11/src/main.cpp
+++ Chapter_05/exercises/5.11/src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 int main()
 {
-   
+    double amount;
+    double principal = 1000.0f;
+
+    std::cout << "Year"
+              << std::setw(8)
+              << "Rate"
+              << std::setw(21)
+              << "Amount on deposit"
+              << std::endl;
+    std::cout << std::fixed
+              << std::setprecision(2);
+
+    for (int rate = 5; rate <= 10; ++rate)
+    {
+        for (int year = 0; year <= 10; ++year)
+        {
+            amount = principal * pow(1.0f + rate / 100.0f, year);
+
+            std::cout << std::setw(4)
+                      << year
+                      << std::setw(8)
+                      << rate / 100.0f
+                      << std::setw(15)
+                      << amount
+                      << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
 
     return 0;
 }
--- Chapter_05/exercises/5.12/src/main.cpp
+++ Chapter_05/exercises/5.12/src/main.cpp
@@ -1,8 +1,104 @@
 #include <iostream>
 
+void printA();
+void printB();
+void printC();
+void printD();
+void printAll();
 int main()
 {
-   
+    printA();
+    printB();
+    printC();
+    printD();
 
+    std::cout << std::endl;
+
+    printAll();
     return 0;
+}
+
+void printA()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        for (int j = 1; j <= 10; ++j)
+        {
+            std::cout << ((j <= i) ? "*" : " ");
+        }
+
+        std::cout << std::endl;
+    }
+}
+void printB()
+{
+    for (int i = 10; i >= 1; --i)
+    {
+        for (int j = 1; j <= 10; ++j)
+        {
+            std::cout << ((j <= i) ? "*" : " ");
+        }
+
+        std::cout << std::endl;
+    }
+}
+void printC()
+{
+    for (int i = 10; i >= 1; --i)
+    {
+        for (int j = 10; j >= 1; --j)
+        {
+            std::cout << ((j <= i) ? "*" : " ");
+        }
+
+        std::cout << std::endl;
+    }
+}
+void printD()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        for (int j = 10; j >= 1; --j)
+        {
+            std::cout << ((j <= i) ? "*" : " ");
+        }
+
+        std::cout << std::endl;
+    }
+}
+void printAll()
+{
+
+    for (int i = 1, j = 10; i <= 10; ++i, --j)
+    {
+        //A
+        for (int k = 1; k <= 10; ++k)
+        {
+            std::cout << ((k <= i) ? "*" : " ");
+        }
+        std::cout << "     ";
+
+        //B
+        for (int k = 1; k <= 10; ++k)
+        {
+            std::cout << ((k <= j) ? "*" : " ");
+        }
+        std::cout << "     ";
+
+        //C
+
+        for (int k = 10; k >= 1; --k)
+        {
+            std::cout << ((k <= j) ? "*" : " ");
+        }
+        std::cout << "     ";
+
+        //D
+
+        for (int k = 10; k >= 1; --k)
+        {
+            std::cout << ((k <= i) ? "*" : " ");
+        }
+        std::cout << std::endl;
+    }
 }
