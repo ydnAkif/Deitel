61d6021f8a93b467ecb1face769612799d166f90
Akif AYDIN <akifaydin52@gmail.com>
2020-08-16 14:30:03 +0300

    Updated

--- Chapter_05/exercises/5.18/src/main.cpp
+++ Chapter_05/exercises/5.18/src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
-   
+    std::cout << "Binary"
+              << std::setw(9)
+              << "Octal"
+              << std::setw(15)
+              << "Hexadecimal"
+              << std::endl;
+    for (int i = 1; i <= 256; ++i)
+    {
+        std::cout << std::setw(3)
+                  << std::dec
+                  << i
+                  << std::setw(9)
+                  << std::oct
+                  << i
+                  << std::setw(15)
+                  << std::hex
+                  << i
+                  << std::endl;
+    }
 
     return 0;
 }
