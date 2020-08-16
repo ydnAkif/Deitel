5aa6914ab75a95719ee47b6767586058d1699392
Akif AYDIN <akifaydin52@gmail.com>
2020-08-16 14:12:28 +0300

    Updated

--- Chapter_05/exercises/5.16/src/main.cpp
+++ Chapter_05/exercises/5.16/src/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 int main()
 {
-   
+    double rate{.05f};
+    int principal{1000};
+
+    std::cout << "Year"
+              << std::setw(21)
+              << "Amount on deposit"
+              << std::endl;
+    std::cout << std::fixed
+              << std::setprecision(2);
+    for (int year = 0; year <= 10; ++year)
+    {
+        int amount = principal * pow(1.0f + rate, year);
+        int dollars = amount / 100;
+        int cents = amount % 100;
+
+        std::cout << std::setw(4)
+                  << year
+                  << std::setw(12)
+                  << dollars
+                  << '.';
+        (cents < 10) ? (std::cout << "0" << cents) : (std::cout << cents);
+        std::cout << std::endl;
+    }
 
     return 0;
 }
