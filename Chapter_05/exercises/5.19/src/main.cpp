cd7f3fc611dbc0e984f2f2e36a904279058aa0db
Akif AYDIN <akifaydin52@gmail.com>
2020-08-17 14:51:17 +0300

    Updated

--- Chapter_05/exercises/5.19/src/main.cpp
+++ Chapter_05/exercises/5.19/src/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
-   
+    int toogle{0};
+    int limit{1000};
+
+    double pi{4.0f};
+    double divisor{3.0f};
+
+    std::cout << std::setw(4)
+              << "Term"
+              << std::setw(13)
+              << "PI Aproax"
+              << std::endl;
+
+    for (int i = 1; i <= limit; ++i)
+    {
+        if (toogle == 0)
+        {
+            pi -= (4.0f / divisor);
+        }
+        else
+        {
+            pi += (4.0f / divisor);
+        }
+
+        toogle = (1 - toogle);
+
+        divisor += 2;
+
+        std::cout << std::setw(4)
+                  << i
+                  << std::setw(13)
+                  << std::fixed
+                  << std::setprecision(6)
+                  << pi
+                  << std::endl;
+    }
 
     return 0;
 }
