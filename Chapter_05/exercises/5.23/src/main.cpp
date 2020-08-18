f04b8f92df360c631a694c80b12c480ea4073e74
Akif AYDIN <akifaydin52@gmail.com>
2020-08-18 12:26:56 +0300

    Updated

--- Chapter_05/exercises/5.23/src/main.cpp
+++ Chapter_05/exercises/5.23/src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
+#include <cmath>
 int main()
 {
-   
+
+    for (int i{1}, j{1}, k{4}; i <= 9; i++)
+    {
+        for (int s{0}; s < k; s++)
+        {
+            std::cout << " ";
+        }
+        for (int d{0}; d < j; d++)
+        {
+            std::cout << "*";
+        }
+        (i >= 5) ? j -= 2 : j += 2;
+        (i >= 5) ? k++ : k--;
+
+        std::cout << std::endl;
+    }
 
     return 0;
 }
