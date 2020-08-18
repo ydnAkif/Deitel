e32398464ba01fe8c0c80edfb1e46db8700951bc
Akif AYDIN <akifaydin52@gmail.com>
2020-08-18 14:24:27 +0300

    Updated

--- Chapter_05/exercises/5.25/src/main.cpp
+++ Chapter_05/exercises/5.25/src/main.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
-   
+    int counter{1};
+    bool exit{false};
+
+    while (!exit)
+    {
+
+        std::cout << counter << " ";
+        ++counter;
+        exit = (counter == 5);
+    }
+
+    std::cout << std::endl
+              << "Broke out of loop at count = " << counter << std::endl;
 
     return 0;
 }
--- Chapter_05/exercises/5.27/src/main.cpp
+++ Chapter_05/exercises/5.27/src/main.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
-   
+    for (int counter = 1; counter <= 10; ++counter)
+    {
+        if (counter != 5)
+        {
+            std::cout << counter << " ";
+        }
+    }
+    std::cout << std::endl
+              << "Skipped printing 5 without continue"
+              << std::endl;
 
     return 0;
 }
