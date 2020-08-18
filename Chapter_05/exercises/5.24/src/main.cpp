1a7d4bb486c919cc412375f29c691fc9b9c652e5
Akif AYDIN <akifaydin52@gmail.com>
2020-08-18 12:49:10 +0300

    Updated

--- Chapter_05/exercises/5.24/src/main.cpp
+++ Chapter_05/exercises/5.24/src/main.cpp
@@ -2,7 +2,42 @@
 
 int main()
 {
-   
+    int rows{0};
+
+    std::cout << "Enter an odd number of rows to display: 1-19:(-1 to quit) ";
+    std::cin >> rows;
+    while (rows != -1)
+    {
+        if (rows <= 19 && rows >= 1 && rows % 2 != 0)
+        {
+            for (int i{1}, j{1}, k{rows / 2}; i <= rows; ++i)
+            {
+                for (int s{0}; s < k; ++s)
+                {
+                    std::cout << " ";
+                }
+
+                for (int d{0}; d < j; ++d)
+                {
+                    std::cout << "*";
+                }
+
+                (i > rows / 2) ? j -= 2 : j += 2;
+                (i > rows / 2) ? k++ : k--;
+
+                std::cout << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << std::endl
+                      << "Incorrect odd number !!!"
+                      << std::endl;
+        }
+        std::cout << std::endl
+                  << "Enter an odd number of rows to display: 1-19:(-1 to quit) ";
+        std::cin >> rows;
+    }
 
     return 0;
 }
