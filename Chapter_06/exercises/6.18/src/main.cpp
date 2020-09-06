7fc5990899b281ff36c1a71aed4ec361481e600c
Akif AYDIN <akifaydin52@gmail.com>
2020-09-06 16:56:22 +0300

    Updated

--- Chapter_06/exercises/6.18/src/main.cpp
+++ Chapter_06/exercises/6.18/src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+int integerPower(int, int);
 
 int main()
 {
+    int base, exponent;
+    std::cout << "A program to get Power"
+              << std::endl
+              << std::endl;
 
+    std::cout << "Enter  base and expnent ";
+    std::cin >> base >> exponent;
 
+    std::cout << base
+              << " ^ "
+              << exponent
+              << " = "
+              << integerPower(base, exponent)
+              << std::endl;
     return 0;
+}
+
+int integerPower(int b, int exp)
+{
+    int power = 1;
+    for (unsigned int i = 1; i <= exp; ++i)
+    {
+        power *= b;
+    }
+    return power;
 }
