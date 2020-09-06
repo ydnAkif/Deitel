6e04e769899958aed22813511ae6357850e7b6be
Akif AYDIN <akifaydin52@gmail.com>
2020-09-06 15:33:33 +0300

    Updated

--- Chapter_06/exercises/6.16/src/main.cpp
+++ Chapter_06/exercises/6.16/src/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
+std::default_random_engine engine(static_cast<int>(time(0)));
+
+int getRandomNumber(const int &, const int &);
 
 int main()
 {
+    std::cout << "Program to assign random integers to variables in the "
+                 "following ranges"
+              << std::endl;
 
+    std::cout << "1 <= n <= 2: "
+              << getRandomNumber(1, 2)
+              << std::endl
+              << "1 <= n <= 100: "
+              << getRandomNumber(1, 100)
+              << std::endl
+              << "0 <= n <= 9: "
+              << getRandomNumber(0, 9)
+              << std::endl
+              << "1000 <= n <= 1112: "
+              << getRandomNumber(1000, 1112)
+              << std::endl
+              << "-1 <= n <= 1: "
+              << getRandomNumber(-1, 1)
+              << std::endl
+              << "-3 <= n <= 11: "
+              << getRandomNumber(-3, 11)
+              << std::endl;
 
     return 0;
+}
+
+int getRandomNumber(const int &min, const int &max)
+{
+    std::uniform_int_distribution<int> randomInt(min, max);
+    return randomInt(engine);
 }
--- Chapter_06/exercises/6.17/src/main.cpp
+++ Chapter_06/exercises/6.17/src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
+std::default_random_engine engine(static_cast<int>(time(0)));
+
+int getRandomNumber(const int &, const int &);
 
 int main()
 {
+    std::cout << "Program to assign random integers to variables in the "
+                 "following ranges"
+              << std::endl;
 
+    std::cout << "2, 4, 6, 8, 10 : " // 2n
+              << getRandomNumber(1, 5) * 2
+              << std::endl
+              << "3, 5, 7, 9, 11 : " //2n+1
+              << getRandomNumber(1, 5) * 2 + 1
+              << std::endl
+              << "6, 10 ,14 , 18 , 22 : " // 4n+2
+              << getRandomNumber(1, 5) * 4 + 2
+              << std::endl;
 
     return 0;
+}
+
+int getRandomNumber(const int &min, const int &max)
+{
+    std::uniform_int_distribution<int> randomInt(min, max);
+    return randomInt(engine);
 }
