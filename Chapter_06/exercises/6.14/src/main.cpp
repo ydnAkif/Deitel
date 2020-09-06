e5a4431ac39044bc7e066c48c4929f5a210c4b15
Akif AYDIN <akifaydin52@gmail.com>
2020-09-06 13:07:47 +0300

    Updated

--- Chapter_06/exercises/6.14/src/main.cpp
+++ Chapter_06/exercises/6.14/src/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <cmath>
 
+double roundToInteger(double);
+double roundToTenths(double);
+double roundToHundreths(double);
+double roundToThousandths(double);
 
 int main()
 {
+    double num{0.0f};
 
+    std::cout << "Program to round numbers to int, 10s, 100 and 1000"
+              << std::endl;
+    std::cout << "Enter number to round (-1 to exit): ";
+    std::cin >> num;
+
+    while (num != -1)
+    {
+        std::cout << "Original: "
+                  << num
+                  << std::endl
+                  << "roundToInteger: "
+                  << roundToInteger(num)
+                  << std::endl
+                  << "roundToTenths: "
+                  << roundToTenths(num)
+                  << std::endl
+                  << "roundToHundredths: "
+                  << roundToHundreths(num)
+                  << std::endl
+                  << "roundToThousandths: "
+                  << roundToThousandths(num)
+                  << std::endl;
+
+        std::cout << "Enter number to round (-1 to exit): ";
+        std::cin >> num;
+    }
 
     return 0;
 }
+
+double roundToInteger(double num)
+{
+    return floor(num + 0.5f);
+}
+double roundToTenths(double num)
+{
+    return floor(num * 10 + 0.5f) / 10;
+}
+double roundToHundreths(double num)
+{
+    return floor(num * 100 + 0.5f) / 100;
+}
+double roundToThousandths(double num) { return floor(num * 1000 + 0.5f) / 1000; }
