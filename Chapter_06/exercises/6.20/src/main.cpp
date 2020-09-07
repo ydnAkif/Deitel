00ba55a8d549f9e8b382c06d64be61a3bfc91119
Akif AYDIN <akifaydin52@gmail.com>
2020-09-07 18:23:34 +0300

    Updated

--- Chapter_06/exercises/6.20/src/main.cpp
+++ Chapter_06/exercises/6.20/src/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
+bool isMultiple(int, int);
 
 int main()
 {
+    int x, y, choice{0};
+    std::cout << "A program to determine if first"
+              << " integer is a multiple of a second "
+              << std::endl;
 
+    while (choice >= 0)
+    {
+        std::cout << "Enter two integer (-1 to quit ) : ";
+        std::cin >> x;
+
+        if (x == -1)
+        {
+            break;
+        }
+
+        std::cin >> y;
+
+        std::cout << x
+                  << " and "
+                  << y
+                  << (isMultiple(x, y) ? ": true" : ": false")
+                  << std::endl;
+    }
 
     return 0;
+}
+
+bool isMultiple(int x, int y)
+{
+    return (y % x == 0);
 }
