93f6ef806a1e8ed1e3aab5bdcc1b3d3992ab0219
Akif AYDIN <akifaydin52@gmail.com>
2020-09-08 07:39:26 +0300

    Updated

--- Chapter_06/exercises/6.21/src/main.cpp
+++ Chapter_06/exercises/6.21/src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-
+bool isEven(int); // inline bool isEven(int x) {return (x%2==0);}
 int main()
 {
+    int input = 0;
 
+    std::cout << "A program to determine whether an integer is even"
+              << std::endl
+              << std::endl;
+
+    while (input >= 0)
+    {
+        std::cout << "Enter an integer (-1 to quit): ";
+        std::cin >> input;
+        if (input >= 0)
+        {
+            std::cout << input
+                      << " : "
+                      << (isEven(input) ? "" : " not")
+                      << " even"
+                      << std::endl;
+        }
+    }
 
     return 0;
+}
+
+bool isEven(int x)
+{
+    return (x % 2 == 0);
 }
