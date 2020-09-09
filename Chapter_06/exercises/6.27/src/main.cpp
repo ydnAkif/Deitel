f2152ee434e3c104ff5f153237c300d2c7b78a79
Akif AYDIN <akifaydin52@gmail.com>
2020-09-09 15:22:53 +0300

    Updated

--- Chapter_06/exercises/6.27/src/main.cpp
+++ Chapter_06/exercises/6.27/src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <iomanip>
 
-
+double minimum(double, double, double);
 int main()
 {
+    double d1, d2, d3;
+    std::cout << "A Program to find the smallest of 3 doubles"
+              << std::endl
+              << std::endl;
 
+    std::cout << "Enter three double values : ";
+    std::cin >> d1 >> d2 >> d3;
+
+    std::cout << std::fixed
+              << std::setprecision(1)
+              << "Smallest: "
+              << minimum(d1, d2, d3)
+              << std::endl;
 
     return 0;
+}
+
+double minimum(double d1, double d2, double d3)
+{
+    double min = d1 < d2 ? d1 : d2;
+    return (d3 < min ? d3 : min);
 }
