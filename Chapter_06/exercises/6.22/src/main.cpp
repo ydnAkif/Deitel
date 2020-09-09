6abde851b152b751e68c1f51771244908e374c0c
Akif AYDIN <akifaydin52@gmail.com>
2020-09-09 13:00:12 +0300

    Updated

--- Chapter_06/exercises/6.22/src/main.cpp
+++ Chapter_06/exercises/6.22/src/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
+void displaySquare(int);
 
 int main()
 {
+    unsigned int side{0};
+    std::cout << "Program to display a square of asterisks of size n"
+              << std::endl
+              << std::endl;
+    std::cout << "Enter the side of square : ";
+    std::cin >> side;
 
+    displaySquare(side);
 
     return 0;
+}
+
+void displaySquare(int n)
+{
+    for (unsigned int row = 1; row <= n; ++row)
+    {
+        for (unsigned int column = 1; column <= n; ++column)
+        {
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+    }
 }
