96c4e3448bf4f5dd96e2a6a87a8d3eb6677556a5
Akif AYDIN <akifaydin52@gmail.com>
2020-09-09 23:25:24 +0300

    Updated

--- Chapter_06/exercises/6.28/src/main.cpp
+++ Chapter_06/exercises/6.28/src/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <iomanip>
 
+bool isPerfect(unsigned long long);
 
 int main()
 {
+    std::cout << "A Program to test whether a number is perfect or not"
+              << std::endl
+              << std::endl;
+    std::cout << "Perfect number between 1 and 10000: "
+              << std::endl;
 
+    for (unsigned long long i = 1; i <= 10'000; ++i)
+    {
+        if (isPerfect(i))
+        {
+            std::cout << std::setw(12)
+                      << i
+                      << " : "
+                      << "\t";
+
+            for (unsigned long long d = 1; d < i; ++d)
+            {
+                if (i % d == 0)
+                {
+                    std::cout << d << " ";
+                }
+            }
+
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
 
     return 0;
+}
+
+bool isPerfect(unsigned long long n)
+{
+    unsigned long long sum{0};
+
+    for (unsigned long long i = 1; i <= (n / 2); ++i)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+
+    return sum == n;
 }
