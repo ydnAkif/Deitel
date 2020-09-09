40295ad726f91e643114177ea18f3c6c91ab95de
Akif AYDIN <akifaydin52@gmail.com>
2020-09-09 13:26:07 +0300

    Updated

--- Chapter_06/exercises/6.24/src/main.cpp
+++ Chapter_06/exercises/6.24/src/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
+void seperateInt(long);
 
 int main()
 {
+    long n;
 
+    std::cout << "A program to print a separated a sequence of integers"
+              << std::endl
+              << std::endl;
+    std::cout << "Enter the multi digit number : ";
+    std::cin >> n;
+
+    seperateInt(n);
 
     return 0;
+}
+
+void seperateInt(long n)
+{
+    long numDigits{1};
+
+    for (long i = n; i > 10; i /= 10)
+    {
+        numDigits *= 10;
+    }
+
+    while (numDigits > 0)
+    {
+        std::cout << (n / numDigits) % 10 << " " ;
+
+        numDigits /= 10;
+    }
+
+    std::cout << std::endl;
 }
