c0fadacab9752263e9eed4bab862c21c11ba546d
Akif AYDIN <akifaydin52@gmail.com>
2020-09-10 17:53:37 +0300

    Updated

--- Chapter_06/exercises/6.30/src/main.cpp
+++ Chapter_06/exercises/6.30/src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 
-
+int reverseDigits(int);
 int main()
 {
+    int n;
 
+    std::cout << "A Program to reverse the digits of a given int"
+              << std::endl
+              << std::endl;
+
+    std::cout << "Enter a number to reversed : ";
+    std::cin >> n;
+
+    std::cout << std::endl
+              << reverseDigits(n)
+              << std::endl;
 
     return 0;
+}
+
+int reverseDigits(int n)
+{
+
+    int reversed;
+    int digits{1};
+
+    for (unsigned int d = 1; d <= n; d *= 10)
+    {
+        digits *= 10;
+    }
+
+    while (n != 0)
+    {
+        digits /= 10;
+        reversed += n % 10 * digits;
+        n /= 10;
+    }
+    return reversed;
 }
