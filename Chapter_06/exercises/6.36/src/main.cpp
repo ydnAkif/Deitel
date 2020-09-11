0feeecd05f4b800b62675950e0cabb97433019cf
Akif AYDIN <akifaydin52@gmail.com>
2020-09-11 16:31:56 +0300

    Updated

--- Chapter_06/exercises/6.36/src/main.cpp
+++ Chapter_06/exercises/6.36/src/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
-
+int power(int, int);
 int main()
 {
+    int base, exponent;
+    std::cout << "A program to recursively calculate exponents"
+              << std::endl;
 
+    std::cout << std::endl
+              << "Enter the base and exponent ";
+    std::cin >> base >> exponent;
 
+    std::cout << base
+              << " to power of "
+              << exponent
+              << " : "
+              << power(base, exponent)
+              << std::endl;
     return 0;
+}
+
+int power(int base, int exponent)
+{
+    if (exponent == 0)
+    {
+        return 1;
+    }
+
+    return (base * power(base, exponent - 1));
 }
--- Chapter_06/exercises/6.37/src/main.cpp
+++ Chapter_06/exercises/6.37/src/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
-
+int fibonacci(int);
 int main()
 {
+    int n;
 
+    std::cout << "A Program to non recursively calculate fibonacci to n"
+              << std::endl;
+    std::cout << "Enter the n: ";
+    std::cin >> n;
 
+    std::cout << "Fibonacci to the "
+              << n
+              << "th power: "
+              << fibonacci(n)
+              << std::endl;
     return 0;
+}
+
+int fibonacci(int n)
+{
+    int firsTerm{0};
+    int secondTerm{1};
+    int fib{0};
+
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        if (i <= 1)
+        {
+            fib = i;
+        }
+        else
+        {
+            fib = firsTerm + secondTerm;
+            firsTerm = secondTerm;
+            secondTerm = fib;
+        }
+    }
+
+    return fib;
 }
