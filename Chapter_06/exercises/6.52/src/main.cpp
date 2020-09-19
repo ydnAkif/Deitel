d6f096985da02ddb5a56a5c56ce39aa587dda474
Akif AYDIN <akifaydin52@gmail.com>
2020-09-19 18:32:29 +0300

    Updated

--- Chapter_06/exercises/6.52/src/main.cpp
+++ Chapter_06/exercises/6.52/src/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 
+template <typename T>
 
+T minimum(T num1, T num2)
+{
+    return (num1 < num2) ? num1 : num2;
+}
 int main()
 {
 
+    std::cout << "Template testing using int, char and float"
+              << std::endl;
+
+    //int
+
+    std::cout << "minimum(91, 78): "
+              << minimum(91, 78)
+              << std::endl;
+
+    //char
+
+    std::cout << "minimum(x, G): "
+              << minimum('x', 'G')
+              << std::endl;
+
+    //int double
+    std::cout << "minimum(61.25, 68.5): "
+              << minimum(61.25, 68.5)
+              << std::endl;
 
     return 0;
 }
--- Chapter_06/exercises/6.53/src/main.cpp
+++ Chapter_06/exercises/6.53/src/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
-
+template <typename T>
+T maximum(T num1, T num2)
+{
+    return (num1 > num2) ? num1 : num2;
+}
 int main()
 {
+    std::cout << "Template testing using int, char and float"
+              << std::endl;
+    std::cout << "maximum(91, 78): "
+              << maximum(91, 78)
+              << std::endl;
 
+    //char
 
+    std::cout << "maximum(x, G): "
+              << maximum('x', 'G')
+              << std::endl;
+
+    //int double
+    std::cout << "maximum(61.25, 68.5): "
+              << maximum(61.25, 68.5)
+              << std::endl;
     return 0;
 }
