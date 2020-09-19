6370e58be6f0c963d20eb41be31e6da6c36e9b98
Akif AYDIN <akifaydin52@gmail.com>
2020-09-19 18:07:56 +0300

    Updated

--- Chapter_06/exercises/6.50/src/main.cpp
+++ Chapter_06/exercises/6.50/src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
+inline int tripleByValue(int count)
+{
+    count = count * count * count;
+    return count;
+}
+
+inline int tripleByReference(int &count)
+{
+    count = count * count * count;
+    return count;
+}
 
 int main()
 {
+    std::cout << "Demonstrating passing parameters by reference and value"
+              << std::endl;
 
+    int count = 3;
+
+    tripleByValue(count);
+
+    std::cout << "tripleByValue = "
+              << count
+              << std::endl;
+
+    tripleByReference(count);
+    std::cout
+        << "tripleByReference = "
+
+        << count
+        << std::endl;
 
     return 0;
 }
