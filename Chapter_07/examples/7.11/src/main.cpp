7f277060847ad5c84d86f7e40b700bc382e1809c
Akif AYDIN <akifaydin52@gmail.com>
2020-09-27 18:07:37 +0300

    Updated

--- Chapter_07/examples/7.11/src/main.cpp
+++ Chapter_07/examples/7.11/src/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
-
+#include <iomanip>
+#include <array>
 
 int main(int argc, char const *argv[])
 {
+    const size_t responseSize = 20;
+    const size_t frequencySize = 6;
 
+    const std::array<unsigned int, responseSize> responses = {1, 2, 5, 4, 3, 5, 2, 1, 3, 1, 4, 3, 3, 3, 2, 3, 3, 2, 2, 5};
 
+    std::array<unsigned int, frequencySize> frequency = {};
 
-    return 0;
+    for (size_t answer = 0; answer < responses.size(); ++answer)
+    {
+        ++frequency[responses[answer]];
+    }
+
+    std::cout << "Rating"
+              << std::setw(17)
+              << "Frequency"
+              << std::endl;
+
+    for (size_t rating = 1; rating < frequency.size();++rating) {
+        std::cout << std::setw(6)
+                  << rating
+                  << std::setw(17)
+                  << frequency[rating]
+                  << std::endl;
+    }
+
+        return 0;
 }
--- Chapter_07/examples/7.12/src/main.cpp
+++ Chapter_07/examples/7.12/src/main.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
+#include <array>
 
+void staticArrayInit();
+void automaticArrayInit();
 
-int main(int argc, char const *argv[])
+const size_t arraySize = 3;
+
+int main()
 {
+    std::cout << "First call to each function:\n";
+    staticArrayInit();
+    automaticArrayInit();
 
+    std::cout << "\n\nSecond call to each function:\n";
+    staticArrayInit();
+    automaticArrayInit();
 
+    std::cout << std::endl;
+}
 
-    return 0;
+void staticArrayInit(void)
+{
+    static std::array<int, arraySize> array1;
+    std::cout << "\nValues on entering staticArrayInit:\n";
+
+    for (size_t i = 0; i < array1.size(); ++i)
+    {
+        std::cout << "array1[" << i << "] = " << array1[i] << " ";
+    }
+
+    std::cout << "\nValues on exiting staticArrayInit:\n";
+
+    for (size_t j = 0; j < array1.size(); ++j)
+    {
+        std::cout << "array1[" << j << "] = " << (array1[j] +=5 )<< " ";
+    }
+    
+}
+
+void automaticArrayInit(void)
+{
+    std::array<int, arraySize> array2 = {1,2,3};
+    std::cout << "\n\nValues on entering automaticArrayInit:\n";
+
+    for (size_t i = 0; i < array2.size(); ++i)
+    {
+        std::cout << "array2[" << i << "] = " << array2[i] << " ";
+    }
+
+    std::cout << "\nValues on exiting automaticArrayInit:\n";
+
+    for (size_t j = 0; j < array2.size(); ++j)
+    {
+        std::cout << "array2[" << j << "] = " << (array2[j] +=5 )<< " ";
+    }
+    
 }
