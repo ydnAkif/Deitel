432196b409c06c20e5380086de67576344d927a6
Akif AYDIN <akifaydin52@gmail.com>
2020-09-27 17:07:21 +0300

    Updated

--- Chapter_07/examples/7.09/src/main.cpp
+++ Chapter_07/examples/7.09/src/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
-
+#include <iomanip>
+#include <array>
 
 int main(int argc, char const *argv[])
 {
 
+    const size_t arraySize = 11;
 
+    std::array<int, arraySize> n = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1};
+
+    std::cout << "Grade distiribution:"
+              << std::endl;
+    for (size_t i = 0; i < n.size(); ++i)
+    {
+        if (0 == i)
+        {
+            std::cout << "  0-9: ";
+        }
+        else if (10 == i)
+        {
+            std::cout << "  100: ";
+        }
+        else
+        {
+            std::cout << i * 10
+                      << "-"
+                      << (i * 10) + 9
+                      << ": ";
+        }
+
+        for (unsigned int stars = 0; stars < n[i]; ++stars)
+        {
+            std::cout << '*';
+        }
+
+        std::cout << std::endl;
+    }
 
     return 0;
 }
--- Chapter_07/examples/7.10/src/main.cpp
+++ Chapter_07/examples/7.10/src/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-
+#include <iomanip>
+#include <array>
+#include <random>
+#include <ctime>
 
 int main(int argc, char const *argv[])
 {
+    std::default_random_engine engine(static_cast<unsigned int>(time(0)));
+    std::uniform_int_distribution<unsigned int> randomInt(1, 6);
 
+    const size_t arraySize = 7;
 
+    std::array<int, arraySize> frequency = {};
+
+    for (unsigned int roll = 1; roll <= 6000000; ++roll)
+    {
+        ++frequency[randomInt(engine)];
+    }
+
+    std::cout << "Face"
+              << std::setw(13)
+              << "Frequency"
+              << std::endl;
+
+    for (size_t face = 1; face < frequency.size(); ++face)
+    {
+        std::cout << std::setw(4)
+                  << face
+                  << std::setw(13)
+                  << frequency[face]
+                  << std::endl;
+    }
 
     return 0;
 }
