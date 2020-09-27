2dceb89980755e9dfb212b751a7662a94b817328
Akif AYDIN <akifaydin52@gmail.com>
2020-09-27 18:52:15 +0300

    Updated

--- Chapter_07/examples/7.13/src/main.cpp
+++ Chapter_07/examples/7.13/src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-
+#include <array>
 
 int main(int argc, char const *argv[])
 {
+    std::array<int, 5> items = {1, 2, 3, 4, 5};
 
+    std::cout << "items before modification: ";
+    for (int item : items)
+        std::cout << item << " ";
 
+    for (int &itemRef : items)
+        itemRef *= 2;
+    std::cout << "\nitems after modification: ";
 
-    return 0;
+    for (int item: items)
+        std::cout << item << " ";
+
+    std::cout << std::endl;
 }
