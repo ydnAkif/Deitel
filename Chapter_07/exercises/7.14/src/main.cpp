c981a45ae10c365b115c4aee74ae6c5924793b9d
Akif AYDIN <akifaydin52@gmail.com>
2020-09-30 19:15:17 +0300

    Updated

--- Chapter_07/exercises/7.14/src/main.cpp
+++ Chapter_07/exercises/7.14/src/main.cpp
@@ -4,56 +4,45 @@
 
 using namespace std;
 
-const int vectorSize = 20;
-
-void addToVector(vector<int> &, int &);
-
 int main(int argc, char const *argv[])
 {
-    vector<int> myVector(vectorSize);
-    int value;
+    const size_t vectorSize = 20;
+    vector<int> myVector;
+    int subscript = 0;
+    int value = 0;
+    bool isDuplicate;
 
     for (unsigned int i = 0; i < vectorSize; ++i)
     {
+        isDuplicate = false;
+
         cout << "(" << i + 1 << "/20) Enter value between 10 and 100: ";
         cin >> value;
 
         if (value >= 10 && value <= 100)
-            addToVector(myVector, value);
+        {
+            for (unsigned int i = 0; i < myVector.size(); ++i)
+            {
+                if (value == myVector[i])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate)
+            {
+                myVector.push_back(value);
+                ++subscript;
+            }
+        }
         else
             i--;
     }
 
-    for (unsigned int i = 0; i < vectorSize; ++i)
-    {
-        if (myVector.at(i) != 0)
-        {
-            cout << myVector.at(i) << setw(4);
-        }
-    }
+    for (unsigned int index = 0; index < subscript; ++index)
+        cout << myVector[index] << setw(4);
     cout << endl;
 
     return 0;
-}
-
-void addToVector(vector<int> &myVector, int &value)
-{
-    static int subscript = 0;
-    bool isDuplicate = false;
-
-    for (unsigned int i = 0; i <= subscript; ++i)
-    {
-        if (myVector.at(i) == value)
-        {
-            isDuplicate = true;
-            break;
-        }
-    }
-
-    if (!isDuplicate)
-    {
-        //myVector.push_back(value);
-        myVector.at(subscript) = value;
-        ++subscript;
-    }
 }
