4b322f26a626463a1d0a5b7a6ddb73caf3c87935
Akif AYDIN <akifaydin52@gmail.com>
2020-10-09 15:55:48 +0300

    Updated

--- Chapter_07/exercises/7.28/src/main.cpp
+++ Chapter_07/exercises/7.28/src/main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+#include <string>
+#include <array>
 
+using namespace std;
 
+const size_t SIZE = 80;
+
+bool testPalindrome(const array<string, SIZE> &, int, int);
 
 int main(int argc, char const *argv[])
 {
+    string text;
+    array<string, SIZE> stringArr, copy;
 
+    int count = 0, copyCount, i;
 
+    cout << "Enter a sentence:\n";
+
+    while ((text = cin.get()) != "\n" && count < SIZE)
+        stringArr[count++] = text;
+
+    for (copyCount = 0, i = 0; stringArr[i] != "\0"; ++i)
+        if (stringArr[i] != " ")
+            copy[copyCount++] = stringArr[i];
+    if (testPalindrome(copy, 0, copyCount - 1))
+    {
+        cout << '\"';
+        for (auto index : stringArr)
+            cout << index;
+        cout << "\" is a palindrome" << endl;
+    }
+    else
+    {
+        cout << '\"';
+        for (auto index : stringArr)
+            cout << index;
+        cout << "\" is not a palindrome" << endl;
+    }
     return 0;
 }
 
+bool testPalindrome(const array<string, SIZE> &arrayString, int left, int right)
+{
+
+    if (left == right || left > right)
+        return true;
+    else if (arrayString[left] != arrayString[right])
+        return false;
+    else
+        return testPalindrome(arrayString, left + 1, right - 1);
+}
--- Chapter_07/exercises/7.30/src/main.cpp
+++ Chapter_07/exercises/7.30/src/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <iomanip>
+#include <array>
+#include <random>
+#include <ctime>
 
+using namespace std;
 
+const size_t SIZE = 20;
+
+default_random_engine engine(static_cast<int>(time(0)));
+uniform_int_distribution<int> randomInt(1, 100);
+
+void printArray(const array<int, SIZE>, int, int);
 
 int main(int argc, char const *argv[])
 {
+    const int MAXNUMBER = 500;
 
+    array<int, SIZE> arr;
+    for (size_t loop = 0; loop < SIZE; ++loop)
+        arr[loop] = randomInt(engine);
+    cout << " Array values printed in main:\n";
+
+    for (size_t j = 0; j < SIZE; ++j)
+        cout << setw(5) << arr[j];
+
+    cout << "\n\nArray values printed in printArray:\n";
+    printArray(arr, 0, SIZE - 1);
+    cout << endl;
 
     return 0;
 }
 
+void printArray(const array<int, SIZE> arr, int low, int high)
+{
+    cout << setw(5) << arr[low];
+    if (low == high)
+        return;
+    else
+        printArray(arr, low + 1, high);
+}
