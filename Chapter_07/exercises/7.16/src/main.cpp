e525c22efb1f12c73bf46ebb57e20b1039fb2a1f
Akif AYDIN <akifaydin52@hotmail.com>
2020-10-24 11:19:05 +0300

    Updated

--- Chapter_07/exercises/7.16/src/main.cpp
+++ Chapter_07/exercises/7.16/src/main.cpp
@@ -8,18 +8,39 @@ using namespace std;
 
 int rollDice();
 
-static int totalRolls = 36000;
-const static size_t total = 13;
+static int totalRolls = 10;
+const static size_t arraySize = 13;
 
 default_random_engine engine(static_cast<int>(time(0)));
 uniform_int_distribution<unsigned int> randomInt(1, 6);
 
 int main()
 {
-    array<int, total> expected = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
-    array<int, total> tally = {};
 
-    cout << "Program to print the totals of 2 dice rolls 36000 times\n"
+    /*
+
+    Expected rolls
+
+    1+1 = 2
+    2+1,1+2 = 3
+    2+2,3+1,1+3 = 4
+    1+4,4+1,2+3,3+2 = 5
+    1+5,5+1,2+4,4+2,3+3 = 6
+    1+6,6+1,2+5,5+2,3+4,4+3 =7
+    2+6,6+2,3+5,5+3,4+4 = 8
+    3+6,6+3,4+5,5+4 = 9
+    4+6,6+4,5+5 = 10
+    5+6,6+5 = 11
+    6+6 = 12
+    
+    */
+    array<int, arraySize> expected = {0, 0, 1, 2, 3, 4,
+                                      5, 6, 5, 4, 3, 2, 1};
+
+    array<int, arraySize> tally = {};
+
+    cout << "Program to print the totals of 2 dice rolls 36000 times"
+         << endl
          << endl;
     for (unsigned int i = 0; i < totalRolls; ++i)
     {
@@ -30,11 +51,11 @@ int main()
          << "Expected" << setw(10) << "Actual" << endl
          << setiosflags(ios::fixed | ios::showpoint);
 
-    for (size_t i = 2; i < expected.size(); ++i)
+    for (size_t i = 2; i < arraySize; ++i)
     {
         cout << setw(10) << i << setw(10) << tally[i] << setprecision(3)
              << setw(9) << 100.0 * expected[i] / 36 << "%" << setprecision(3)
-             << setw(9) << 100.0 * tally[i] / 36000 << "%" << endl;
+             << setw(9) << 100.0 * tally[i] / totalRolls << "%" << endl;
     }
 
     return 0;
