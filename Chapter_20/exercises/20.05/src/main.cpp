42437940d2b77c6ae8df34ebf9800c3aaa3016e8
Akif AYDIN <akifaydin52@hotmail.com>
2021-03-07 20:19:07 +0300

    Updated

---
+++ Chapter_20/exercises/20.05/src/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <array>
+#include <ctime>
+#include <random>
+
+template <typename T, size_t size>
+void bubbleSort(std::array<T, size> &items)
+{
+    for (size_t i = 1; i < items.size(); ++i)
+    {
+        for (size_t j = 0; j < items.size() - i; ++j)
+        {
+            if (items[j] > items[j + 1])
+            {
+                T temp = items[j];
+                items[j] = items[j + 1];
+                items[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::default_random_engine engine(static_cast<unsigned int>(time(nullptr)));
+    std::uniform_int_distribution<unsigned int> randomInt(10, 99);
+
+    const size_t arraySize = 10;
+    std::array<int, arraySize> data;
+
+    for (int &item : data)
+        item = randomInt(engine);
+
+    std::cout << "Before sorting:\n";
+
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        std::cout << data[i] << ' ';
+    }
+    std::cout << std::endl;
+    clock_t startTime = clock(), endTime;
+
+    bubbleSort(data);
+
+    endTime = clock();
+    std::cout << (double)(endTime - startTime) / CLOCKS_PER_SEC << " seconds passed.";
+    std::cout << "\nAfter sorting:\n";
+
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        std::cout << data[i] << ' ';
+    }
+
+    return 0;
+}
